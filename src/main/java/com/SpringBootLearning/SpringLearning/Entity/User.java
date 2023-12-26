6f54991307f3512e2603e7f681e0bc344ea545fe
raju <rajupraaa1234@gmail.com>
2023-12-27 00:39:00 +0530

    My code

---
+++ src/main/java/com/SpringBootLearning/SpringLearning/Entity/User.java
@@ -0,0 +1,68 @@
+package com.SpringBootLearning.SpringLearning.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+    private String city;
+    private String status;
+
+    public User() {
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User(int id, String name, String city, String status) {
+        this.id = id;
+        this.name = name;
+        this.city = city;
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", city='" + city + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
---
+++ src/main/java/com/SpringBootLearning/SpringLearning/SpringLearningApplication.java
@@ -0,0 +1,24 @@
+package com.SpringBootLearning.SpringLearning;
+
+import com.SpringBootLearning.SpringLearning.Dao.UserRepository;
+import com.SpringBootLearning.SpringLearning.Entity.User;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringLearningApplication {
+
+	public static void main(String[] args) {
+		System.out.println("application started ...");
+		ApplicationContext context =  SpringApplication.run(SpringLearningApplication.class, args);
+		UserRepository repository = (UserRepository) context.getBean(UserRepository.class);
+		User user = new User();
+		user.setName("Raju kumar");
+		user.setCity("Hyderabad");
+		user.setStatus("Processing");
+		User response = repository.save(user);
+		System.out.println(response);
+	}
+
+}
