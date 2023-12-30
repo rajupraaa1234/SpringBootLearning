d56248ea0380134405b1bd75f56c71d12e8ba1a3
raju <rajupraaa1234@gmail.com>
2023-12-31 01:04:32 +0530

    ResponseEntity added

--- src/main/java/com/SpringBootLearning/SpringLearning/Controler/BooksControler.java
+++ src/main/java/com/SpringBootLearning/SpringLearning/Controler/BooksControler.java
@@ -4,10 +4,13 @@ import com.SpringBootLearning.SpringLearning.Entity.Book;
 import com.SpringBootLearning.SpringLearning.Services.BookServices;
 import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class BooksControler {
@@ -35,21 +38,37 @@ public class BooksControler {
         return bookServices.getBooksList();
     }
 
+    // Here ResponseEntity is a generic type which is used for http error code handling
     @GetMapping("/books/{id}")
-    public Book getBookById(@PathVariable("id") int id){
-        return bookServices.getBookById(id);
+    public ResponseEntity<Book>  getBookById(@PathVariable("id") int id){
+        Book book = null;
+        try{
+            book = bookServices.getBookById(id);
+        }catch (Exception e){
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+        if(book == null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.of(Optional.of(book));
     }
 
     @PostMapping("/book")
-    public Book addBook(@RequestBody Book book){
+    public ResponseEntity<Book> addBook(@RequestBody Book book){
         bookServices.addBooks(book);
-        return book;
+        return ResponseEntity.ok(book);
     }
 
     @DeleteMapping("/book/{id}")
-    public int deleteBook(@PathVariable("id") int id){
-        bookServices.deleteBook(id);
-        return id;
+    public ResponseEntity<Void> deleteBook(@PathVariable("id") int id){
+        try{
+            bookServices.deleteBook(id);
+           return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }catch (Exception error){
+            error.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
     @PutMapping("/book/{id}")
